aced775c5d0eb90449122b07c4a7c030bd89bb9c
MuhammadHaziq <muhammadhaziq341@gmail.com>
2023-05-16 23:03:58 +0500

    update product structure

--- app/models/Products/index.js
+++ app/models/Products/index.js
@@ -6,7 +6,7 @@ const ProductSchema = new Schema(
         product_name: { type: String, required: true, trim: true, index: true },
         product_price: { type: Number, required: true, trim: true },
         product_quantity: { type: Number, required: false, trim: true, default:null },
-        user_id:{type: Schema.Types.ObjectId, ref: 'User', required:true},
+        created_by:{type: Schema.Types.ObjectId, ref: 'User', required:true},
         updated_by:{type: Schema.Types.ObjectId, ref: 'User', default:null},
         deleted_by:{type: Schema.Types.ObjectId, ref: 'User', default:null},
         deleted_at:{ type: Date, default: null}
