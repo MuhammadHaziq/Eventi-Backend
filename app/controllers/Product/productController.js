a770080f2215052f176bd031ce07adbee890d875
MuhammadHaziq <muhammadhaziq341@gmail.com>
2023-05-21 19:08:16 +0500

    add pagination on products

--- app/controllers/Product/productController.js
+++ app/controllers/Product/productController.js
@@ -1,63 +1,103 @@
 const productService = require("../../services/Product/productService");
 
 module.exports = {
-    addProduct:async(req, res) => {
-        try{
-            const body = {...req.body, ...req.params, user_id:req.user_id};
-            const newProduct = await productService.addProduct(body);
-            if(newProduct) return helper.apiResponse(res, false, "Product Created Successfully", newProduct);
-            return helper.apiResponse(res, true, "Product Not Created Successfully", null);
-        }catch(err){
-            const statusCode = err.status || 'INTERNAL_SERVER_ERROR';
-            return helper.apiResponse(res, true, err.message, null, statusCode);
-        }
-    },
+  addProduct: async (req, res) => {
+    try {
+      const body = { ...req.body, ...req.params, user_id: req.user_id };
+      const newProduct = await productService.addProduct(body);
+      if (newProduct)
+        return helper.apiResponse(
+          res,
+          false,
+          "Product Created Successfully",
+          newProduct
+        );
+      return helper.apiResponse(
+        res,
+        true,
+        "Product Not Created Successfully",
+        null
+      );
+    } catch (err) {
+      const statusCode = err.status || "INTERNAL_SERVER_ERROR";
+      return helper.apiResponse(res, true, err.message, null, statusCode);
+    }
+  },
 
-    getProducts:async(req, res) => {
-        try{
-            const body = {...req.body, ...req.params, user_id:req.user_id};
-            const products = await productService.getProducts(body);
-            if(products && products?.length > 0) return helper.apiResponse(res, false, "Products Fetch Successfully", products);
-            return helper.apiResponse(res, true, "No Products Found", null);
-        }catch(err){
-            const statusCode = err.status || 'INTERNAL_SERVER_ERROR';
-            return helper.apiResponse(res, true, err.message, null, statusCode);
-        }
-    },
+  getProducts: async (req, res) => {
+    try {
+      const body = {
+        ...req.body,
+        ...req.params,
+        ...req.query,
+        user_id: req.user_id,
+      };
+      const products = await productService.getProducts(body);
+      if (products && products?.data?.length > 0)
+        return helper.apiResponse(
+          res,
+          false,
+          "Products Fetch Successfully",
+          products
+        );
+      return helper.apiResponse(res, true, "No Products Found", null);
+    } catch (err) {
+      const statusCode = err.status || "INTERNAL_SERVER_ERROR";
+      return helper.apiResponse(res, true, err.message, null, statusCode);
+    }
+  },
 
-    getProduct:async(req, res) => {
-        try{
-            const body = {...req.body, ...req.params, user_id:req.user_id};
-            const product = await productService.getProduct(body);
-            if(product) return helper.apiResponse(res, false, "Product Fetch Successfully", product);
-            return helper.apiResponse(res, true, "No Product Found", null);
-        }catch(err){
-            const statusCode = err.status || 'INTERNAL_SERVER_ERROR';
-            return helper.apiResponse(res, true, err.message, null, statusCode);
-        }
-    },
+  getProduct: async (req, res) => {
+    try {
+      const body = { ...req.body, ...req.params, user_id: req.user_id };
+      const product = await productService.getProduct(body);
+      if (product)
+        return helper.apiResponse(
+          res,
+          false,
+          "Product Fetch Successfully",
+          product
+        );
+      return helper.apiResponse(res, true, "No Product Found", null);
+    } catch (err) {
+      const statusCode = err.status || "INTERNAL_SERVER_ERROR";
+      return helper.apiResponse(res, true, err.message, null, statusCode);
+    }
+  },
 
-    updateProduct:async(req, res) => {
-        try{
-            const body = {...req.body, ...req.params, user_id:req.user_id};
-            const updatedProduct = await productService.updateProduct(body);
-            if(updatedProduct) return helper.apiResponse(res, false, "Product Updated Successfully", updatedProduct);
-            return helper.apiResponse(res, true, "No Product Found", null);
-        }catch(err){
-            const statusCode = err.status || 'INTERNAL_SERVER_ERROR';
-            return helper.apiResponse(res, true, err.message, null, statusCode);
-        }
-    },
+  updateProduct: async (req, res) => {
+    try {
+      const body = { ...req.body, ...req.params, user_id: req.user_id };
+      const updatedProduct = await productService.updateProduct(body);
+      if (updatedProduct)
+        return helper.apiResponse(
+          res,
+          false,
+          "Product Updated Successfully",
+          updatedProduct
+        );
+      return helper.apiResponse(res, true, "No Product Found", null);
+    } catch (err) {
+      const statusCode = err.status || "INTERNAL_SERVER_ERROR";
+      return helper.apiResponse(res, true, err.message, null, statusCode);
+    }
+  },
 
-    deleteProduct:async(req, res) => {
-        try{
-            const body = {...req.body, ...req.params, user_id:req.user_id};
-            const deletedProduct = await productService.deleteProduct(body);
-            if(deletedProduct) return helper.apiResponse(res, false, "Product Deleted Successfully", deletedProduct);
-            return helper.apiResponse(res, true, "No Product Found", null);
-        }catch(err){
-            const statusCode = err.status || 'INTERNAL_SERVER_ERROR';
-            return helper.apiResponse(res, true, err.message, null, statusCode);
-        }
-    },
-}
+  deleteProduct: async (req, res) => {
+    try {
+      const body = { ...req.body, ...req.params, user_id: req.user_id };
+      const deletedProduct = await productService.deleteProduct(body);
+      if (deletedProduct)
+        return helper.apiResponse(
+          res,
+          false,
+          "Product Deleted Successfully",
+          deletedProduct
+        );
+      return helper.apiResponse(res, true, "No Product Found", null);
+    } catch (err) {
+      const statusCode = err.status || "INTERNAL_SERVER_ERROR";
+      return helper.apiResponse(res, true, err.message, null, statusCode);
+    }
+  },
+};
--- app/services/Product/productService.js
+++ app/services/Product/productService.js
@@ -1,49 +1,94 @@
 const appConfig = require("../../../config/appConfig");
-const Product = require("../../models/Products")
+const Product = require("../../models/Products");
 
 const error = new Error();
-error.status = 'NOT_FOUND';
+error.status = "NOT_FOUND";
 error.message = null;
 error.data = null;
+const productFilters = (filters, user_id) => {
+  if (filters) {
+    if (filters.product_name) {
+      filters.product_name = { $regex: filters.product_name, $options: "i" };
+    } else {
+      delete filters.product_name;
+    }
+    if (!filters.product_price) {
+      delete filters.product_price;
+    }
+    if (!filters.product_quantity) {
+      delete filters.product_quantity;
+    }
+  }
+
+  return { ...filters, created_by: user_id, deleted_at: null };
+};
 
 module.exports = {
+  addProduct: async (body) => {
+    const { product_name, product_price, product_quantity, user_id } = body;
+    const newProduct = new Product({
+      product_name,
+      product_price,
+      product_quantity,
+      created_by: user_id,
+    });
+    return await newProduct.save();
+  },
 
-    addProduct:async (body)=> {
-        const {product_name, product_price, product_quantity, user_id} = body;
-        const newProduct  = new Product({
-            product_name,
-            product_price,
-            product_quantity,
-            created_by:user_id
-        });
-        return await newProduct.save();
-    },
+  getProducts: async (body) => {
+    const { user_id, perPage, page, tableFilters, sort } = body;
+    const sorter = sort ? JSON.parse(sort) : null;
+    const filters = tableFilters ? JSON.parse(tableFilters) : null;
+    const totalRecord = await Product.find(
+      productFilters(filters, user_id)
+    ).count();
+    const startIndex = ((page || 1) - 1) * (perPage || 10);
+    const tableRows = helper.pagination(totalRecord, page || 1, perPage || 10);
+    const record = await Product.find(productFilters(filters, user_id))
+      .sort({ [sorter?.value || "createdAt"]: sorter?.state || -1 })
+      .skip(startIndex)
+      .limit(perPage || 10)
+      .lean();
+    tableRows.data = record;
+    return tableRows;
+  },
 
-    getProducts:async (body)=> {
-        const {user_id} = body;
-        return await Product.find({created_by:user_id, deleted_at:null}).sort({ createdAt: -1 }).lean();
-        
-    },
+  getProduct: async (body) => {
+    const { user_id, product_id } = body;
+    return await Product.findOne({
+      _id: product_id,
+      created_by: user_id,
+      deleted_at: null,
+    }).lean();
+  },
 
-    getProduct:async (body)=> {
-        const {user_id, product_id} = body;
-        return await Product.findOne({_id:product_id, created_by:user_id, deleted_at:null}).lean();
-        }, 
+  updateProduct: async (body) => {
+    const {
+      product_name,
+      product_price,
+      product_quantity,
+      user_id,
+      product_id,
+    } = body;
+    const updatedProduct = await Product.findOneAndUpdate(
+      { _id: product_id, deleted_at: null },
+      {
+        product_name,
+        product_price,
+        product_quantity,
+        updated_by: user_id,
+      },
+      { new: true }
+    ).lean();
+    return updatedProduct;
+  },
 
-    updateProduct:async (body) => {
-        const {product_name, product_price, product_quantity, user_id, product_id} = body;
-       const updatedProduct =  await Product.findOneAndUpdate({ _id: product_id, deleted_at:null }, {
-            product_name,
-            product_price,
-            product_quantity,
-            updated_by:user_id,
-        }, { new: true }).lean();
-        return updatedProduct;
-    },
-
-    deleteProduct:async (body)=> {
-        const {user_id, product_id} = body;
-        return await Product.findOneAndUpdate({_id:product_id, deleted_at:null},{deleted_by:user_id, deleted_at:new Date()}, {new:true}).lean();
-    }, 
-
-}
+  deleteProduct: async (body) => {
+    const { user_id, product_id } = body;
+    return await Product.findOneAndUpdate(
+      { _id: product_id, deleted_at: null },
+      { deleted_by: user_id, deleted_at: new Date() },
+      { new: true }
+    ).lean();
+  },
+};
