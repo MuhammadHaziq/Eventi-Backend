f74d2409d6c6ab2ac7f705cb6b421151d9247a91
MuhammadHaziq <muhammadhaziq341@gmail.com>
2023-05-22 00:40:20 +0500

    update curd

--- app/controllers/Vendor/vendorController.js
+++ app/controllers/Vendor/vendorController.js
@@ -1,63 +1,123 @@
 const vendorService = require("../../services/Vendor/vendorServices");
 
 module.exports = {
-    addVendor:async(req, res)=> {
-        try{
-            const body = {...req.body, ...req.params, user_id:req.user_id};
-            const newVendor = await vendorService.addVendor(body);
-            if(newVendor) return helper.apiResponse(res, false, "Vendor Created Successfully", newVendor);
-            return helper.apiResponse(res, true, "Vendor Not Created Successfully", null);
-        }catch(err){
-            const statusCode = err.status || 'INTERNAL_SERVER_ERROR';
-            return helper.apiResponse(res, true, err.message, null, statusCode);
-        }
-    },
+  addVendor: async (req, res) => {
+    try {
+      const body = { ...req.body, ...req.params, user_id: req.user_id };
+      const newVendor = await vendorService.addVendor(body);
+      if (newVendor)
+        return helper.apiResponse(
+          res,
+          false,
+          "Vendor Created Successfully",
+          newVendor
+        );
+      return helper.apiResponse(
+        res,
+        true,
+        "Vendor Not Created Successfully",
+        null
+      );
+    } catch (err) {
+      const statusCode = err.status || "INTERNAL_SERVER_ERROR";
+      return helper.apiResponse(res, true, err.message, null, statusCode);
+    }
+  },
 
-    getVendors:async(req, res) => {
-        try{
-            const body = {...req.body, ...req.params, user_id:req.user_id};
-            const vendors = await vendorService.getVendors(body);
-            if(vendors) return helper.apiResponse(res, false, "Vendors Fetch Successfully", vendors);
-            return helper.apiResponse(res, true, "Vendors Not Fetch Successfully", null);
-        }catch(err){
-            const statusCode = err.status || 'INTERNAL_SERVER_ERROR';
-            return helper.apiResponse(res, true, err.message, null, statusCode);
-        }
-    },
+  getVendors: async (req, res) => {
+    try {
+      const body = {
+        ...req.body,
+        ...req.params,
+        ...req.query,
+        user_id: req.user_id,
+      };
+      const vendors = await vendorService.getVendors(body);
+      if (vendors)
+        return helper.apiResponse(
+          res,
+          false,
+          "Vendors Fetch Successfully",
+          vendors
+        );
+      return helper.apiResponse(
+        res,
+        true,
+        "Vendors Not Fetch Successfully",
+        null
+      );
+    } catch (err) {
+      const statusCode = err.status || "INTERNAL_SERVER_ERROR";
+      return helper.apiResponse(res, true, err.message, null, statusCode);
+    }
+  },
 
-    getVendor:async(req, res) => {
-        try{
-            const body = {...req.body, ...req.params, user_id:req.user_id};
-            const vendor = await vendorService.getVendor(body);
-            if(vendor) return helper.apiResponse(res, false, "Vendor Fetch Successfully", vendor);
-            return helper.apiResponse(res, true, "Vendor Not Fetch Successfully", null);
-        }catch(err){
-            const statusCode = err.status || 'INTERNAL_SERVER_ERROR';
-            return helper.apiResponse(res, true, err.message, null, statusCode);
-        }
-    },
+  getVendor: async (req, res) => {
+    try {
+      const body = { ...req.body, ...req.params, user_id: req.user_id };
+      const vendor = await vendorService.getVendor(body);
+      if (vendor)
+        return helper.apiResponse(
+          res,
+          false,
+          "Vendor Fetch Successfully",
+          vendor
+        );
+      return helper.apiResponse(
+        res,
+        true,
+        "Vendor Not Fetch Successfully",
+        null
+      );
+    } catch (err) {
+      const statusCode = err.status || "INTERNAL_SERVER_ERROR";
+      return helper.apiResponse(res, true, err.message, null, statusCode);
+    }
+  },
 
-    updateVendor:async(req, res) => {
-        try{
-            const body = {...req.body, ...req.params, user_id:req.user_id};
-            const updatedVendor = await vendorService.updateVendor(body);
-            if(updatedVendor) return helper.apiResponse(res, false, "Vednor Updated Successfully", updatedVendor);
-            return helper.apiResponse(res, true, "Vednor Not Updated Successfully", null);
-        }catch(err){
-            const statusCode = err.status || 'INTERNAL_SERVER_ERROR';
-            return helper.apiResponse(res, true, err.message, null, statusCode);
-        }
-    },
+  updateVendor: async (req, res) => {
+    try {
+      const body = { ...req.body, ...req.params, user_id: req.user_id };
+      const updatedVendor = await vendorService.updateVendor(body);
+      if (updatedVendor)
+        return helper.apiResponse(
+          res,
+          false,
+          "Vednor Updated Successfully",
+          updatedVendor
+        );
+      return helper.apiResponse(
+        res,
+        true,
+        "Vednor Not Updated Successfully",
+        null
+      );
+    } catch (err) {
+      const statusCode = err.status || "INTERNAL_SERVER_ERROR";
+      return helper.apiResponse(res, true, err.message, null, statusCode);
+    }
+  },
 
-    deleteVendor:async(req, res) => {
-        try{
-            const body = {...req.body, ...req.params, user_id:req.user_id};
-            const deletedVendor = await vendorService.deleteVendor(body);
-            if(deletedVendor) return helper.apiResponse(res, false, "Vednor Deleted Successfully", deletedVendor);
-            return helper.apiResponse(res, true, "Vednor Not Deleted Successfully", null);
-        }catch(err){
-            const statusCode = err.status || 'INTERNAL_SERVER_ERROR';
-            return helper.apiResponse(res, true, err.message, null, statusCode);
-        }
-    },
-}
+  deleteVendor: async (req, res) => {
+    try {
+      const body = { ...req.body, ...req.params, user_id: req.user_id };
+      const deletedVendor = await vendorService.deleteVendor(body);
+      if (deletedVendor)
+        return helper.apiResponse(
+          res,
+          false,
+          "Vednor Deleted Successfully",
+          deletedVendor
+        );
+      return helper.apiResponse(
+        res,
+        true,
+        "Vednor Not Deleted Successfully",
+        null
+      );
+    } catch (err) {
+      const statusCode = err.status || "INTERNAL_SERVER_ERROR";
+      return helper.apiResponse(res, true, err.message, null, statusCode);
+    }
+  },
+};
